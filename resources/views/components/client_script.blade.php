b0cb3f9bec4c181fdb14ec1b72cf8c85e38e9ac0
ThaeSuAung20 <thaesuaung552000@gmail.com>
2023-10-17 09:59:51 +0630

    Search, Choose Brand and Profile Update

--- resources/views/components/client_script.blade.php
+++ resources/views/components/client_script.blade.php
@@ -1,35 +1,52 @@
 <!-- script section section start  -->
-  <!-- bootstrap css1 js1 -->
-  <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- bootstrap css1 js1 -->
+<script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
-  <script src="{{ asset('bootstrap/js/proper.min.js') }}"></script>
-  <!-- <script src="./vendor/bootstrap/js/bootstrap.min.js"></script> -->
+<script src="{{ asset('bootstrap/js/proper.min.js') }}"></script>
+<!-- <script src="./vendor/bootstrap/js/bootstrap.min.js"></script> -->
 
-  <!-- jquery -->
-  <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+<!-- jquery -->
+<script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
 
-  <!-- owl carousel  -->
-  <script src="{{ asset('owl-carousel/js/owl.carousel.min.js') }}"></script>
+<!-- owl carousel  -->
+<script src="{{ asset('owl-carousel/js/owl.carousel.min.js') }}"></script>
 
-  <!-- custom js -->
-  <script src="{{ asset('js/app.js') }}"></script>
-  <!-- script section section end  -->
+<!-- custom js -->
+<script src="{{ asset('js/app.js') }}"></script>
+<!-- script section section end  -->
 
-  <script>
 
-  //landing page carousel
+<script>
+document.getElementById("category").addEventListener("change", function() {
+ var selectedOption = this.options[this.selectedIndex];
+ if (selectedOption.value !== "choose") {
+  var selectedValue = selectedOption.value;
+  if (selectedValue === "all") {
+   var url = "{{ url('/shop') }}"; // Redirect to the URL for showing all products
+  } else {
+   var url = `{{ url('/shop/brands') }}/${selectedValue}`;
+  }
+  window.location.href = url;
+ }
+});
+</script>
 
-  $(document).ready(function () {
-  // Initialize Landing Page Carousel
-  var landingPageCarousel = $('#landing-page-carousel');
-  landingPageCarousel.owlCarousel({
-    items: 1,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 3000,
-  });
 
-  // Other code specific to the landing page carousel
+
+<script>
+//landing page carousel
+
+$(document).ready(function() {
+ // Initialize Landing Page Carousel
+ var landingPageCarousel = $('#landing-page-carousel');
+ landingPageCarousel.owlCarousel({
+  items: 1,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 3000,
+ });
+
+ // Other code specific to the landing page carousel
 
 });
 
@@ -62,116 +79,116 @@
 //     });
 // });
 
-$(document).ready(function () {
-  var productDetailsCarousel = $('#product-details-carousel');
-  var modal = $('#carousel-modal');
-  var modalCarousel = $('#modal-carousel');
-  var currentItemIndex = 0; // Track the currently displayed item index
-  var clonedItems = null; // Store cloned items
+$(document).ready(function() {
+ var productDetailsCarousel = $('#product-details-carousel');
+ var modal = $('#carousel-modal');
+ var modalCarousel = $('#modal-carousel');
+ var currentItemIndex = 0; // Track the currently displayed item index
+ var clonedItems = null; // Store cloned items
 
-  // Initialize the main Owl Carousel for product details
-  productDetailsCarousel.owlCarousel({
-    items: 4,
-    loop: true,
-    margin: 10,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-    nav: false,
+ // Initialize the main Owl Carousel for product details
+ productDetailsCarousel.owlCarousel({
+  items: 4,
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  nav: false,
+ });
+
+ // Initialize the modal Owl Carousel for manual navigation
+ modalCarousel.owlCarousel({
+  items: 1,
+  loop: true,
+  autoplay: false,
+  nav: true,
+  dots: true,
+ });
+
+ // Handle click on a carousel item to open the modal
+ productDetailsCarousel.on('click', '.item', function() {
+  var itemId = $(this).data('item-id');
+  var items = productDetailsCarousel.find('.item');
+
+  // Find the index of the selected item and set it as the current item index
+  items.each(function(index) {
+   if ($(this).data('item-id') === itemId) {
+    currentItemIndex = index;
+    return false; // Exit the loop
+   }
   });
 
-  // Initialize the modal Owl Carousel for manual navigation
-  modalCarousel.owlCarousel({
-    items: 1,
-    loop: true,
-    autoplay: false,
-    nav: true,
-    dots: true,
-  });
+  // If clonedItems is null, clone the items from productDetailsCarousel
+  if (clonedItems === null) {
+   clonedItems = items.clone();
+  }
 
-  // Handle click on a carousel item to open the modal
-  productDetailsCarousel.on('click', '.item', function () {
-    var itemId = $(this).data('item-id');
-    var items = productDetailsCarousel.find('.item');
+  // Clear the modal carousel and add the selected item
+  modalCarousel.trigger('replace.owl.carousel', [clonedItems]);
 
-    // Find the index of the selected item and set it as the current item index
-    items.each(function(index) {
-      if ($(this).data('item-id') === itemId) {
-        currentItemIndex = index;
-        return false; // Exit the loop
-      }
-    });
+  // Set the current item in the modal carousel
+  modalCarousel.trigger('to.owl.carousel', [currentItemIndex, 0, true]);
 
-    // If clonedItems is null, clone the items from productDetailsCarousel
-    if (clonedItems === null) {
-      clonedItems = items.clone();
-    }
+  // Show the modal
+  modal.css('display', 'block');
+ });
 
-    // Clear the modal carousel and add the selected item
-    modalCarousel.trigger('replace.owl.carousel', [clonedItems]);
+ // Handle next button click in the modal
+ modal.on('click', '.owl-next', function() {
+  currentItemIndex++;
+  modalCarousel.trigger('to.owl.carousel', [currentItemIndex, 0, true]);
+ });
 
-    // Set the current item in the modal carousel
-    modalCarousel.trigger('to.owl.carousel', [currentItemIndex, 0, true]);
+ // Handle previous button click in the modal
+ modal.on('click', '.owl-prev', function() {
+  currentItemIndex--;
+  modalCarousel.trigger('to.owl.carousel', [currentItemIndex, 0, true]);
+ });
 
-    // Show the modal
-    modal.css('display', 'block');
-  });
-
-  // Handle next button click in the modal
-  modal.on('click', '.owl-next', function () {
-    currentItemIndex++;
-    modalCarousel.trigger('to.owl.carousel', [currentItemIndex, 0, true]);
-  });
-
-  // Handle previous button click in the modal
-  modal.on('click', '.owl-prev', function () {
-    currentItemIndex--;
-    modalCarousel.trigger('to.owl.carousel', [currentItemIndex, 0, true]);
-  });
-
-  // Close the modal when the close button or outside the modal is clicked
-  modal.on('click', function (e) {
-    if (e.target === modal[0] || $(e.target).hasClass('close')) {
-      modal.css('display', 'none');
-    }
-  });
+ // Close the modal when the close button or outside the modal is clicked
+ modal.on('click', function(e) {
+  if (e.target === modal[0] || $(e.target).hasClass('close')) {
+   modal.css('display', 'none');
+  }
+ });
 });
 
 
 
 // Common code that doesn't depend on the carousels can stay outside of the above blocks
-$(document).ready(function () {
-  $("#nav > ul > li.dropdowns").click(function (e) {
-    if ($(window).width() > 1400) {
-      $(this).parent("ul").parent('div').siblings('div').fadeToggle(150);
-      e.preventDefault();
-    }
-  });
+$(document).ready(function() {
+ $("#nav > ul > li.dropdowns").click(function(e) {
+  if ($(window).width() > 1400) {
+   $(this).parent("ul").parent('div').siblings('div').fadeToggle(150);
+   e.preventDefault();
+  }
+ });
 
-  // Other common code
+ // Other common code
 });
 
 
 
-    // Get the input element
-    const numberInput = document.getElementById("numberInput");
+// Get the input element
+const numberInput = document.getElementById("numberInput");
 
-    // Function to decrease the value
-    function decreaseValue() {
-      if (numberInput.value > 1) {
-        numberInput.value--;
-      }
-    }
+// Function to decrease the value
+function decreaseValue() {
+ if (numberInput.value > 1) {
+  numberInput.value--;
+ }
+}
 
-    // Function to increase the value
-    function increaseValue() {
-      if (numberInput.value < 10) {
-        numberInput.value++;
-      }
-    }
+// Function to increase the value
+function increaseValue() {
+ if (numberInput.value < 10) {
+  numberInput.value++;
+ }
+}
 
 
-    $(document).ready(function () {
-      $('#getyear').text(new Date().getUTCFullYear());
-    });
-  </script>
+$(document).ready(function() {
+ $('#getyear').text(new Date().getUTCFullYear());
+});
+</script>
