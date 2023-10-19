8a778dfedc2e0d50ac98966a154aa7c52baaea5c
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2023-10-19 09:03:41 +0630

    authenthecation added by amk

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -10,13 +10,16 @@
 		     <span class="menu-text">Dashboard</span>
 		    </a>
 		   </div>
+					@can('storage_access')
 		   <div class="menu-item">
 		    <a href="analytics.html" class="menu-link">
 		     <span class="menu-icon"><i class="bi bi-bar-chart"></i></span>
 		     <span class="menu-text">Analytics</span>
 		    </a>
 		   </div>
+					@endcan
 		   <div class="menu-item has-sub">
+						@can('complete_order_management_access')
 		    <a href="#" class="menu-link">
 		     <span class="menu-icon">
 		      <i class="bi bi-envelope"></i>
@@ -24,26 +27,34 @@
 		     <span class="menu-text">Orders</span>
 		     <span class="menu-caret"><b class="caret"></b></span>
 		    </a>
+						@endcan
 		    <div class="menu-submenu">
 		     <div class="menu-item">
+								@can('pending_order_access')
 		      <a href="{{ url('/admin/orders/pending/') }}" class="menu-link">
 		       <span class="menu-text">Pending Orders</span>
 		      </a>
+								@endcan
 		     </div>
 		     <div class="menu-item">
+								@can('delivering_order_access')
 		      <a href="{{ url('/admin/orders/delivering/') }}" class="menu-link">
 		       <span class="menu-text">Delivering Orders</span>
 		      </a>
+								@endcan
 		     </div>
 		     <div class="menu-item">
+								@can('complete_order_access')
 		      <a href="{{ url('/admin/orders/completed/') }}" class="menu-link">
 		       <span class="menu-text">Completed Orders</span>
 		      </a>
+								@endcan
 		     </div>
 		    </div>
 		   </div>
 		   <div class="menu-header">Authentications</div>
 		   <div class="menu-item has-sub">
+						@can('user_management_access')
 		    <a href="javascript:;" class="menu-link">
 		     <div class="menu-icon">
 		      <i class="bi bi-bag-check"></i>
@@ -52,30 +63,37 @@
 		     <div class="menu-text d-flex align-items-center">UserManagement</div>
 		     <span class="menu-caret"><b class="caret"></b></span>
 		    </a>
+						@endcan
 		    <div class="menu-submenu">
 		     <div class="menu-item">
+								@can('permission_access')
 		      <a href="{{ route('admin.permissions.index') }}" class="menu-link">
 		       <div class="menu-text">Permission</div>
 		      </a>
+								@endcan
 		     </div>
 		     <div class="menu-item">
+								@can('role_access')
 		      <a href="{{ route('admin.roles.index') }}" class="menu-link">
 		       <div class="menu-text">Role</div>
 		      </a>
+								@endcan
 		     </div>
 		     <div class="menu-item">
+								@can('user_access')
 		      <a href="{{ route('admin.users.index') }}" class="menu-link">
 		       <div class="menu-text">Users</div>
 		      </a>
+								@endcan
 		     </div>
 		     <div class="menu-item">
 		      <a href="pos_table_booking.html" class="menu-link">
-		       <div class="menu-text">Table Booking</div>
+		       {{-- <div class="menu-text">Table Booking</div> --}}
 		      </a>
 		     </div>
 		     <div class="menu-item">
 		      <a href="pos_menu_stock.html" class="menu-link">
-		       <div class="menu-text">Menu Stock</div>
+		       {{-- <div class="menu-text">Menu Stock</div> --}}
 		      </a>
 		     </div>
 		    </div>
@@ -84,6 +102,7 @@
 		   {{-- User Interface --}}
 		   <div class="menu-header">User Interface</div>
 		   <div class="menu-item has-sub">
+						@can('banner_management_access')
 		    <a href="javascript:;" class="menu-link">
 		     <div class="menu-icon">
 		      <i class="bi bi-layout-sidebar"></i>
@@ -91,17 +110,21 @@
 		     <div class="menu-text d-flex align-items-center">Home Page</div>
 		     <span class="menu-caret"><b class="caret"></b></span>
 		    </a>
+						@endcan
 		    <div class="menu-submenu">
 		     <div class="menu-item">
+								@can('banner_access')
 		      <a href="{{ route('admin.banners.index') }}" class="menu-link">
 		       <div class="menu-text">Home Banners</div>
 		      </a>
+								@endcan
 		     </div>
 		    </div>
 		   </div>
 		   {{-- User Interface --}}
 
 		   {{-- Product Management --}}
+					@can('product_management_access')
 		   <div class="menu-header">Product Management</div>
 		   <div class="menu-item has-sub">
 		    <a href="javascript:;" class="menu-link">
@@ -114,52 +137,71 @@
 		      <b class="caret"></b>
 		     </span>
 		    </a>
+						@endcan
 		    <div class="menu-submenu">
 		     <div class="menu-item">
+								@can('product_access')
 		      <a href="{{ route('admin.products.index') }}" class="menu-link">
 		       <div class="menu-text">Products</div>
 		      </a>
+								@endcan
 		     </div>
 		     <div class="menu-item">
+								@can('product_access')
 		      <a href="{{ route('admin.product_prices.index') }}" class="menu-link">
 		       <div class="menu-text">Product Stocks</div>
 		      </a>
+								@endcan
 		     </div>
 		     <div class="menu-item">
+								@can('accessory_access')
 		      <a href="{{ route('admin.accessories.index') }}" class="menu-link">
 		       <div class="menu-text">Accessories</div>
 		      </a>
+								@endcan
 		     </div>
 		     <div class="menu-item">
+								@can('accessory_category_access')
 		      <a href="{{ route('admin.accessory_categories.index') }}" class="menu-link">
 		       <div class="menu-text">Accessory Categories</div>
 		      </a>
+								@endcan
 		     </div>
 		     <div class="menu-item">
+								@can('category_access')
 		      <a href="{{ route('admin.categories.index') }}" class="menu-link">
 		       <div class="menu-text">Categories</div>
 		      </a>
+								@endcan
 		     </div>
 		     <div class="menu-item">
+								@can('brand_access')
 		      <a href="{{ route('admin.brands.index') }}" class="menu-link">
 		       <div class="menu-text">Brands</div>
 		      </a>
+								@endcan
 		     </div>
 
 		     <div class="menu-item">
+								@can('color_access')
 		      <a href="{{ route('admin.colors.index') }}" class="menu-link">
 		       <div class="menu-text">Color</div>
 		      </a>
+								@endcan
 		     </div>
 		     <div class="menu-item">
+								@can('storage_access')
 		      <a href="{{ route('admin.storages.index') }}" class="menu-link">
 		       <div class="menu-text">Storage</div>
 		      </a>
+								@endcan
 		     </div>
 		     <div class="menu-item">
+								@can('ram_access')
 		      <a href="{{ route('admin.rams.index') }}" class="menu-link">
 		       <div class="menu-text">RAM</div>
 		      </a>
+								@endcan
 		     </div>
 		    </div>
 		   </div>
@@ -188,6 +230,7 @@
 		     <span class="menu-text">Calendar</span>
 		    </a>
 		   </div>
+					@can('storage_access')
 		   <div class="menu-item">
 		    <a href="settings.html" class="menu-link">
 		     <span class="menu-icon"><i class="bi bi-gear"></i></span>
@@ -207,6 +250,7 @@
 		    <i class="fa fa-code-branch me-2 ms-n2 opacity-5"></i> Documentation
 		   </a>
 		  </div>
+				@endcan
 		 </div>
 		 <!-- END scrollbar -->
 		</div>
